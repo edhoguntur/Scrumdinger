dbc3bf0a4033f569ec83d927a92a9a09a759eacd
edhoguntur <edho.guntur@gmail.com>
2023-02-07 14:03:11 +0700

    fixed theme color orange and add DailyScrum Model

--- Scrumdinger/Models/Theme.swift
+++ Scrumdinger/Models/Theme.swift
@@ -14,7 +14,7 @@ enum Theme: String {
     case lavender
     case magenta
     case navy
-    case Orange
+    case orange
     case oxblood
     case periwinkle
     case poppy
@@ -27,7 +27,7 @@ enum Theme: String {
     
     var accentColor: Color {
         switch self {
-        case .bubblegum, .buttercup, .lavender, .Orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
+        case .bubblegum, .buttercup, .lavender, .orange, .periwinkle, .poppy, .seafoam, .sky, .tan, .teal, .yellow: return .black
         case .indigo, .magenta, .navy, .oxblood, .purple: return .white
         }
     }
